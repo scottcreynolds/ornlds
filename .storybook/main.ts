2fb40328dcc9e5c44762adb01dacb60f2cf46a9e
Michael Glenn <dmichaelglenn@gmail.com>
2024-11-04 11:17:50 -0800

    Initial commit - migrating work to clean repo

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from "@storybook/web-components-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-a11y"
+  ],
+  framework: {
+    name: "@storybook/web-components-vite",
+    options: {},
+  },
+};
+export default config;
